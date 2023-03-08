103ecf230e3d57868f89618778ffcaf3d62961f7
Faiiss <33278@ma-web.nl>
2023-03-08 12:33:46 +0100

    Save functie gefixed

--- cookieclicker/js/main.js
+++ cookieclicker/js/main.js
@@ -1,82 +1,110 @@
-class Cookie{
+class Cookie {
     name = "";
     htmlElement = undefined;
     factor = 1;
     //Dit wordt 1x uitgevoerd wanneer "new" wordt gebruikt
-    constructor(newName, newHTMLElement, newScore){
+    constructor(newName, newHTMLElement, newScore) {
         this.name = newName;
         this.htmlElement = newHTMLElement;
         this.htmlElement.onclick = this.onCookieClicked;
         this.score = newScore;
     }
 
-    onCookieClicked = () =>{
+    onCookieClicked = () => {
         this.score.onCookieClicked(this.factor);
     }
 
-    onStyleChange(){
+    onStyleChange() {
         this.htmlElement.classList.add("cookie--chocolate");
     }
 
-    onStyleChangesecond(){
+    onStyleChangesecond() {
         this.htmlElement.classList.remove("cookie--chocolate");
         this.htmlElement.classList.add("cookie--velvet");
     }
 }
 
-class Score{
+class Score {
     score;
     name = "";
     htmlElement = undefined;
 
-    constructor(newScore, newName, newHTMLElement){
+    constructor(newScore, newName, newHTMLElement) {
         this.score = newScore;
         this.name = newName;
         this.htmlElement = newHTMLElement;
         this.htmlElement.innerText = newScore;
+
+        // Load saved score from local storage
+        const savedScore = localStorage.getItem("score");
+        if (savedScore !== null) {
+            this.score = parseInt(savedScore);
+            this.htmlElement.innerText = this.score;
+        }
     }
 
-    onCookieClicked(factorFromCookie){
+    onCookieClicked(factorFromCookie) {
         this.score = this.score + factorFromCookie;
         this.htmlElement.innerText = this.score;
 
-       
+        // Save score to local storage
+        this.saveScore();
     }
 
-    subtractScore(){
+    subtractScore() {
         this.score = this.score - 100;
         this.htmlElement.innerText = this.score;
+
+        // Save score to local storage
+        this.saveScore();
     }
 
-    onAutoScoreClicked(){
-        setInterval( () => {
+    onAutoScoreClicked() {
+        setInterval(() => {
             this.score = this.score + 500;
             this.htmlElement.innerText = this.score;
-        }, 1000)
+
+            // Save score to local storage
+            this.saveScore();
+        }, 1000);
     }
 
-    addPoints(){
+    addPoints() {
         this.score = this.score + 10000;
         this.htmlElement.innerText = this.score;
+
+        // Save score to local storage
+        this.saveScore();
+    }
+
+    saveScore() {
+        localStorage.setItem("score", this.score);
+    }
+
+    loadScore() {
+        const savedScore = localStorage.getItem("score");
+        if (savedScore !== null) {
+            this.score = parseInt(savedScore);
+            this.htmlElement.innerText = this.score;
+        }
     }
 }
 
 
-
-class Multiplier{
+class Multiplier {
     factor = 100;
     htmlElement = undefined;
     cookie = undefined;
     bought = false;
 
-    constructor(htmlElement, cookie){
+    constructor(htmlElement, cookie) {
         this.htmlElement = htmlElement;
         this.cookie = cookie;
         this.htmlElement.onclick = this.onMultiplierClicked;
     }
 
     onMultiplierClicked = () => {
-        if(this.bought === false){
+        if (this.bought === false) {
             this.bought = true;
             //remove 100 points from score
             this.cookie.score.subtractScore();
@@ -85,41 +113,41 @@ class Multiplier{
     }
 }
 
-class AutoScore{
+class AutoScore {
     htmlElement = undefined;
     score = undefined;
     forward = false;
 
-    constructor(htmlElement){
+    constructor(htmlElement) {
         this.htmlElement = htmlElement;
         this.score = score;
         this.htmlElement.onclick = this.onAutoScoreClicked;
     }
 
     onAutoScoreClicked = () => {
-        if(this.forward === false){
+        if (this.forward === false) {
             this.forward = true;
             this.score.subtractScore();
             score.onAutoScoreClicked();
         }
-        
+
     }
 }
 
-class ChocolateCookie{
+class ChocolateCookie {
     htmlElement = undefined;
     bought = false;
     forward = false;
     cookie = undefined;
 
-    constructor(htmlElement,cookie){
+    constructor(htmlElement, cookie) {
         this.htmlElement = htmlElement;
         this.cookie = cookie;
         this.htmlElement.onclick = this.onChocolateCookieClicked;
     }
 
     onChocolateCookieClicked = () => {
-        if(this.bought === false){
+        if (this.bought === false) {
             this.bought = true;
             this.cookie.onStyleChange();
             this.cookie.score.addPoints();
@@ -127,20 +155,20 @@ class ChocolateCookie{
     }
 }
 
-class VelvetCookie{
+class VelvetCookie {
     htmlElement = undefined;
     bought = false;
     forward = false;
     cookie = undefined;
 
-    constructor(htmlElement,cookie){
+    constructor(htmlElement, cookie) {
         this.htmlElement = htmlElement;
         this.cookie = cookie;
         this.htmlElement.onclick = this.onVelvetCookieClicked;
     }
 
     onVelvetCookieClicked = () => {
-        if(this.bought === false){
+        if (this.bought === false) {
             this.bought = true;
             this.cookie.onStyleChangesecond();
             this.cookie.score.addPoints();
@@ -148,11 +176,23 @@ class VelvetCookie{
     }
 }
 
+/*setup for score save
+const saveButton = document.getElementById("js--save");
+saveButton.addEventListener("click", () => {
+    localStorage.setItem("score", score.score);
+});
+const savedScore = localStorage.getItem("score");
+if (savedScore !== null) {
+    score.score = parseInt(savedScore);
+    score.htmlElement.innerText = savedScore;
+}*/
 
 
 /*setup for score and cookie*/
 const score = new Score(0, "Default Score", document.getElementById("js--score"));
+score.loadScore(); // Load saved score from local storage
 const cookie = new Cookie("Default Cookie", document.getElementById("js--cookie"), score);
+
 
 /*setup for desktop upgrades*/
 const multiply = new Multiplier(document.getElementById("js--multiplier"), cookie);
