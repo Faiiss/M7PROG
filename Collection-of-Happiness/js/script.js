08fcf33234543097d841a81f49a4f176809cdacd
Faiiss <33278@ma-web.nl>
2023-04-13 18:07:56 +0200

    Elementen komen uit de javaScript

--- Collection-of-Happiness/js/script.js
+++ Collection-of-Happiness/js/script.js
@@ -1,55 +1,90 @@
-// class Podcast {
-//     constructor() {
-//       this.episodes = [];
-//       this.podcastList = document.querySelector(".podcast-list");
-//     }
-  
-//     async init() {
-//       try {
-//         const response = await fetch("data/data.json");
-//         if (!response.ok) {
-//           throw new Error("Network response was not ok");
-//         }
-//         const data = await response.json();
-//         this.episodes = data.episodes;
-//         this.renderEpisodes();
-//       } catch (error) {
-//         console.error("Error fetching podcast data:", error);
-//       }
-//     }
-  
-//     renderEpisodes() {
-//       if (this.episodes.length === 0) {
-//         console.warn("Geen podcast gevonden");
-//         return;
-//       }
-  
-//       // selecteer willekeurige afleveringen uit de lijst
-//       let randomEpisodes = [];
-//       while (randomEpisodes.length < 4) {
-//         let randomIndex = Math.floor(Math.random() * this.episodes.length);
-//         let randomEpisode = this.episodes[randomIndex];
-//         if (!randomEpisodes.includes(randomEpisode)) {
-//           randomEpisodes.push(randomEpisode);
-//         }
-//       }
-  
-//       // voeg de afleveringen toe aan het linkerdeel van de pagina als podcast-kaartjes
-//       randomEpisodes.forEach(episode => {
-//         const podcastCard = document.createElement("div");
-//         podcastCard.classList.add("podcast-card");
-//         podcastCard.innerHTML = `
-//           <img src="${episode.image}" alt="${episode.title}">
-//           <div class="podcast-details">
-//             <p class="podcast-date">${episode.date}</p>
-//             <h3 class="podcast-title">${episode.title}</h3>
-//           </div>
-//         `;
-//         this.podcastList.appendChild(podcastCard);
-//       });
-//     }
-//   }
-  
-//   const podcast = new Podcast();
-//   podcast.init();
-  
+// Select the element to append the podcast tiles to
+const podcastTiles = document.querySelector('.podcasts__tiles');
+
+// Fetch the data from the JSON file
+fetch('/data/data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Shuffle the array of podcast episodes
+    const shuffledEpisodes = shuffleArray(data.episodes);
+
+    // Loop through the first 4 shuffled episodes and create a tile for each one
+    for (let i = 0; i < 4; i++) {
+      const episode = shuffledEpisodes[i];
+
+      // Create the tile element and its contents
+      const tile = document.createElement('li');
+      tile.classList.add('podcasts__tile');
+      tile.innerHTML = `
+        <div class="podcasts__tile-right">
+          <img src="${episode.img}" alt="${episode.title}">
+        </div>
+        <div class="podcasts__tile-left">
+          <div class="podcasts__tile-date">${formatDate(episode["date (dd-mm-yyyy)"])}</div>
+          <div class="podcasts__tile-title">${episode.title}</div>
+        </div>
+      `;
+
+      // Append the tile to the podcast tiles element
+      podcastTiles.appendChild(tile);
+    }
+  })
+  .catch(error => console.error(error));
+
+// Function to shuffle an array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// Function to format the date in the "dd-mm-yyyy" format
+function formatDate(dateString) {
+  const dateParts = dateString.split("-");
+  const year = dateParts[2];
+  const month = dateParts[1];
+  const day = dateParts[0];
+  return `${day}-${month}-${year}`;
+}
+
+// Select the featured podcast element
+const featuredPodcast = document.querySelector('.podcasts__featured');
+
+// Fetch the data from the JSON file
+fetch('/data/data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Get the first episode from the data
+    const episode = data.episodes[0];
+
+    // Set the contents of the featured podcast element
+    featuredPodcast.innerHTML = `
+      <div class="podcasts__featured-left">
+        <div class="podcasts__featured-right">
+          <img src="${episode.img}" alt="${episode.title}">
+        </div>
+        <div class="podcasts__featured-date">${formatDate(episode["date (dd-mm-yyyy)"])}</div>
+        <div class="podcasts__featured-title">${episode.title}</div>
+        <p>${episode.summary}</p>
+        <audio controls>
+          <source src="${episode.audio}" type="audio/mpeg">
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    `;
+  })
+  .catch(error => console.error(error));
+
+// Function to format the date in the "dd-mm-yyyy" format
+function formatDate(dateString) {
+    const dateParts = dateString.split("-");
+    const year = dateParts[2];
+    const month = dateParts[1];
+    const day = dateParts[0];
+    return `${day}-${month}-${year}`;
+  }
+
+
+
